3d9396e92f6b0846fb1ccf1cca3caf7f68c5b549
beechaah <beechaah@mail.uc.edu>
2025-12-07 15:15:06 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { PlayerProvider } from "./context/PlayerContext";
-import "./index.css"; 
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <PlayerProvider>
         <App />
       </PlayerProvider>
