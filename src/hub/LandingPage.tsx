1bb052e7b43af86520aa7a859dfcdbb90ae384ae
beechaah <beechaah@mail.uc.edu>
2025-12-07 11:21:36 -0500

    Update LandingPage.tsx

---
+++ src/hub/LandingPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PlayerNameForm from "../components/PlayerNameForm";
+
+export default function LandingPage() {
+  return (
+    <main>
+      <PlayerNameForm />
+    </main>
+  );
+}
