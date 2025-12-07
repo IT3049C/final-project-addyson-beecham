ef2c8ed551a18da106724cf36783fe71fcd9930e
beechaah <beechaah@mail.uc.edu>
2025-12-07 13:38:45 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command, mode }) => ({
+  base: mode === "production" ? "/final-project-addyson-beecham/" : "/",
   plugins: [react()],
   server: { port: 5173, open: false },
   preview: { port: 5173 },
-});
+}));
