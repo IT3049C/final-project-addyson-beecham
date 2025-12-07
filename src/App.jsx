929a1f4eb094ff723476e2929b477f987354a4e4
beechaah <beechaah@mail.uc.edu>
2025-12-07 11:14:02 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import LandingPage from "./hub/LandingPage";
 
-function App() {
+// Games
+import RockPaperScissors from "./games/RockPaperScissors/RPS";
+import TicTacToe from "./games/TicTacToe/TicTacToe";
+import Wordle from "./games/Wordle/Wordle";
+import Hangman from "./games/Hangman/Hangman";
 
+export default function App() {
   return (
     <>
-      <h1>GameHub</h1>
-      <p>Welcome to GameHub</p>
-    </>
-  )
-}
+      <Navbar />
+      <main className="container">
+        <Routes>
+          {/* Hub landing page */}
+          <Route path="/" element={<LandingPage />} />
 
-export default App
+          {/* Games */}
+          <Route path="/rps" element={<RockPaperScissors />} />
+          <Route path="/tictactoe" element={<TicTacToe />} />
+          <Route path="/wordle" element={<Wordle />} />
+          <Route path="/hangman" element={<Hangman />} />
+        </Routes>
+      </main>
+    </>
+  );
+}
